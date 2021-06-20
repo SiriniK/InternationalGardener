2d51aafd409d392d75b3c2c756e1ba6b6080cde4
Sirini Karunadasa <85049699+SiriniK@users.noreply.github.com>
2021-06-19 22:25:05 -0500

    Add files via upload

---
+++ readFireBase.swift
@@ -0,0 +1,80 @@
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+import Combine 
+import FirebaseFirestore
+import Firebase
+
+func readFirebaseData(){
+   self.db.collection("GardeningStuff").getDocuments { (snapshot, err) in
+   if let err = err {
+   	print("Error getting documents: \(err)")
+   } 
+   return snapshot!.documents
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Report Samples document Skill Gap reporting by manager
+Profanity FilterTeam Competitions
+
+
+
+10 Do you have any additional Out Of Box Features?  - Please see attacched NetExam Features.pdf
+13 Describe what type of user-to-user engagement/interaction is supported in the platform? This is key for internal sales training to 
+foster competition, but eventually important for external partner training.
+Attach ensemba screenshot
+
+15 What abilities does your platform offer for in-person training and live virtual classroom training? Can you create/send registration open/close dates, 
+waitlists, confirmations/cancellations, prerequisites? Do you support a calendar of available sessions? 
+
+ILT Screenshot
+Class Page Tabs
+
+16 Does the LMS allow for physical classroom and equipment scheduling? Do you support a calendar of available sessions/how 
+do users find in-person training and live virtual training sessions and register for said session? 
+How are these linked back to the system for completion and validation?
+
+Resource View 
+Instructor Schedukig
+Calendert
+Mobile App - QR Code
+
+
+23 Does the LMS offer alert or notification options? If yes, please describe what types of alerts and/or notifications are offered. Can users select their notification preferences?
+E mail comm list
+
+24 What communication/marketing automation capabilities can be provided via your platform?
+v7 Email Modukle
+
+26
+Rewards screenshot
+
+32 Pathing
+What kind of “pathing” does your platform provide? Does the platform support curriculum-based content and multiple linked courses?
+
+34 Reporting Automation
+
+35  Please provide sample reporting showcasing specifically which metrics are tracked. Ie, completions per course/asset, downloads/asset, avg quiz scores, abandonment rates, etc. Sliced by user, by company, by asset, by language, by time period, etc. Is reporting done in batches or real time?
+
+
+36 Do you have reporting that will identify skills gaps, or ways to understand behavioral changes in learner course work?
+
+
+52 Native Mobile Apps
+Do you have a native mobile app that can be offered in the Apple and Google Play stores? 
+
+
+
+ 
