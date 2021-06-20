7cce044e64b9920c4af81c28c43f3bb28a5d6ce5
Sirini Karunadasa <85049699+SiriniK@users.noreply.github.com>
2021-06-19 22:18:49 -0500

    Add files via upload

---
+++ Camera.swift
@@ -0,0 +1,76 @@
+//
+//  Camera.swift
+//  laungage
+//
+//  Created by Sirini Karunadasa on 6/19/21.
+//
+
+import Foundation
+
+import UIKit
+import Photos
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var cameraButton: UIButton!
+    @IBOutlet weak var cameraPreview: UIImageView!
+    
+   
+    
+    var imagePickerController = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePickerController.delegate = self
+        checkPermissions()
+    }
+
+    @IBAction func tappedCameraButton(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    @IBAction func tappedLibraryButton(_ sender: Any) {
+        self.imagePickerController.sourceType = .photoLibrary
+        self.present(self.imagePickerController, animated: true, completion: nil)
+    }
+    
+    func checkPermissions() {
+        if PHPhotoLibrary.authorizationStatus() != PHAuthorizationStatus.authorized {
+            PHPhotoLibrary.requestAuthorization({ (status: PHAuthorizationStatus) -> Void in ()
+            })
+        }
+
+        if PHPhotoLibrary.authorizationStatus() == PHAuthorizationStatus.authorized {
+        } else {
+            PHPhotoLibrary
+                .requestAuthorization(requestAuthorizationHandler)
+        }
+    }
+    
+    func requestAuthorizationHandler(status: PHAuthorizationStatus) {
+        if PHPhotoLibrary.authorizationStatus() == PHAuthorizationStatus.authorized {
+            print("Camera Enabled")
+        } else {
+            print("Camera Disabled")
+        }
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if picker.sourceType == .photoLibrary {
+        
+        }
+        else {
+            cameraPreview?.image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage
+        }
+        
+        
+        picker.dismiss(animated: true, completion: nil)
+}
+
+}
